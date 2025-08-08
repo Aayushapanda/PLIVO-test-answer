da5fa2eaf0181e0e2f6c7b3d19e06af9128ca1fb
Aayushapanda <96729623+Aayushapanda@users.noreply.github.com>
2025-08-08 21:07:52 +0530

    Delete playground_groq_vercel directory

--- playground_groq_vercel/src/components/Navbar.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export default function Navbar() {
-  return <nav style={{padding: '1rem', background: '#333', color: '#fff'}}>Groq Playground</nav>;
-}
--- playground_groq_vercel/src/pages/Home.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export default function Home() {
-  return <div style={{padding: '2rem'}}>Welcome to Groq Playground</div>;
-}
